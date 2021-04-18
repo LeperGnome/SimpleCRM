bc15b799c56162360ec792de65f70af214f605aa
Konstantin Antonov <tino.ant.s@gmail.com>
2021-04-18 11:41:23 +0300

    minor changes

--- SimpleCRM/wsgi.py
+++ SimpleCRM/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from SimpleCRM.settings import get_settings_module
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', get_settings_module())
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
