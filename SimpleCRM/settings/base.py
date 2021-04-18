749eadd235b56007775a443c92ddcac82ee87cc8
Konstantin Antonov <tino.ant.s@gmail.com>
2021-04-18 03:29:27 +0300

    swagger added

--- SimpleCRM/settings/base.py
+++ SimpleCRM/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,16 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+}
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
 }
 
 ROOT_URLCONF = 'SimpleCRM.urls'
--- SimpleCRM/urls.py
+++ SimpleCRM/urls.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
 
 from SimpleCRM.settings.base import VER
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version=f'v{VER}',
+      description="Simple CRM API",
+      contact=openapi.Contact(email="tino.ant.s@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f'api/v{VER}/', include('core.urls'))
+    path(f'api/v{VER}/', include('core.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
